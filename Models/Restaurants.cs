879aac926379d0c8bacec71a460cd5111b0fab88
mvhohmann <mvhohmann1@buffs.wtamu.edu>
2023-02-11 12:50:37 -0600

    Initial Create

---
+++ Models/Restaurants.cs
@@ -0,0 +1,11 @@
+using System;
+
+  namespace Assignment2
+  {
+    //this is a model of the Restaurants
+      public class restaurants
+      {
+        public int RestaurantsID {get; set;}// This is the "primary key"
+        public int AddressID {get; set;}
+      }
+  }
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment_2___Online_Grocery_Membership_Prototype_Cardinal_.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
